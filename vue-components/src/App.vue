731f6e56307829f958d6a4fd41db599e6d9fffbf
moskwajacek <moskwajacek@gmail.com>
2018-09-08 12:57:11 +0200

    [#3.1] task 3.1 ready

---
+++ vue-components/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id="app">
+    <h2>My list</h2>
+    <items-list :products="products"></items-list>
+    <add-product @add-product="onAddProduct"></add-product>
+  </div>
+</template>
+
+<script>
+import ItemsList from './components/ItemsList';
+import AddProduct from './components/AddProduct';
+
+import uuid from 'uuid/v4';
+
+export default {
+  name: 'app',
+  components: {
+    ItemsList,
+    AddProduct
+  },
+  data() {
+    return {
+      products: [{
+        id: 0,
+        name: 'Jacek'
+      }, {
+        id: 1,
+        name: 'Szymon'
+      }]
+    }
+  },
+  methods: {
+    onAddProduct(product) {
+      this.products.push(product);
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ vue-components/src/components/ItemsList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="p in products" :key="p.id">{{ p.name }}</li>
+    </ul>
+    <p v-if="!products.length">No item!</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ItemsList",
+    props: {
+      products: {
+        type: Array,
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
